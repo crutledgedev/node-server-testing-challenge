6a7c48fcf8e3da24ad14357d204abad2bd89a8a9
Chance Rutledge <chancerutledge2@gmail.com>
2020-03-27 07:54:53 -0700

    minor fix

--- api/server.spec.js
+++ api/server.spec.js
@@ -5,7 +5,7 @@ const server = require("./server");
 
 describe("server.js", () => {
     describe("GET /", () => {
-        it("should return a response of 200", async function(){
+        it("should return a response of 200 OK", async function(){
             const response = await request(server).get("/");
 
             expect(response.status).toBe(200);
